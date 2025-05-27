9e3b22e6206e8d52d7f4c01f0c3aa21dd252cb71
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-05-27 23:12:40 +0300

    Refactor test structure and add missing test cases

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
 console.log (message)
+/*
+describe('what-is-a-test', () =
+  describe('Name', () => {
+    it('returns "Susan"', () =>
+      expect(name).toEqual('Sus
+    })
+  })
+
+
+  describe('Height', () => {
+    it('is less than 40', () =>
+      expect(height).toBeLessTh
+    })
+  })
+
+  describe('Message', () => {
+    it('gives the name and heig
+      expect(message).toInclude
+      expect(message).toInclude
+    })
+  })
+})
+*/
